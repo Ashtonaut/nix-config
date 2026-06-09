06107e0d14f13f5f139b72e60ea93a32dd90833e
Ashtonaut <jzqq6560@leeds.ac.uk>
2026-06-09 09:01:04 +0100

    Add Home Manager

--- flake.nix
+++ flake.nix
@@ -3,12 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.ashtonaut-laptop = nixpkgs.lib.nixosSystem {
       modules = [ 
         ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.ashtonaut = ./home.nix;
+        }
       ];
     };
   };
