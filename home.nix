e91119bc07dec1fbdff91867bfe23dcd980005b1
Ashtonaut <jzqq6560@leeds.ac.uk>
2026-06-19 14:23:39 +0100

    Add monitor settings

--- home.nix
+++ home.nix
@@ -19,6 +19,14 @@
       portalPackage = null;
 
       settings = {
+        monitor = [
+          {
+            output = "eDP-1";
+            mode = "1920x1080@60";
+            position = "0x0";
+            scale = 1;
+          }
+        ];
         bind = 
           let
             mod = "SUPER";
