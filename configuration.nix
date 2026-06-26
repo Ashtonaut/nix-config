db5a89947ea68ec77c8b9dbe17e7e50df3d85287
Ashtonaut <jzqq6560@leeds.ac.uk>
2026-06-26 16:32:44 +0100

    Add Nix store settings

--- configuration.nix
+++ configuration.nix
@@ -99,5 +99,15 @@
 
   system.stateVersion = "25.11";
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix = {
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      auto-optimise-store = true;
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
 }
