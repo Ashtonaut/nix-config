684fb26d3351e22063abae5cb6105ac6721ea5b4
Ashtonaut <jzqq6560@leeds.ac.uk>
2026-07-17 22:41:55 +0100

    Switch to weekly Nix store optimisation

--- configuration.nix
+++ configuration.nix
@@ -144,12 +144,12 @@
         "nix-command"
         "flakes"
       ];
-      auto-optimise-store = true;
     };
     gc = {
       automatic = true;
       dates = "weekly";
       options = "--delete-older-than 30d";
     };
+    optimise.automatic = true;
   };
 }
