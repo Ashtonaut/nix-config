32da581c6cf556049eefd19a288ea63b6ed2bad7
Ashtonaut <jzqq6560@leeds.ac.uk>
2026-07-17 22:35:03 +0100

    Fix agenix inputs

--- flake.nix
+++ flake.nix
@@ -13,12 +13,18 @@
     };
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
     };
     agenix = {
       url = "github:ryantm/agenix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+        darwin.follows = "";
+      };
     };
     git-hooks = {
       url = "github:cachix/git-hooks.nix";
