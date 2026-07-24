f658293a56fa59971503f2ab708cf49837f29aee
Ashtonaut <jzqq6560@leeds.ac.uk>
2026-07-24 10:45:26 +0100

    Add fonts

--- system/configuration.nix
+++ system/configuration.nix
@@ -66,6 +66,22 @@
   i18n.defaultLocale = "en_GB.UTF-8";
   time.timeZone = "Europe/London";
   console.useXkbConfig = true;
+  fonts = {
+    packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
+      inter
+      noto-fonts
+      noto-fonts-color-emoji
+    ];
+    fontconfig.defaultFonts = {
+      monospace = [ "JetBrainsMono Nerd Font" ];
+      sansSerif = [
+        "Inter"
+        "Noto Sans"
+      ];
+      emoji = [ "Noto Color Emoji" ];
+    };
+  };
 
   hardware.bluetooth = {
     enable = true;
